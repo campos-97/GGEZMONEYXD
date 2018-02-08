6fd280b0e9c107f61c871c9f5ed6b2586d89512d
campos-97 <joseacampos97@gmail.com>
2018-02-07 21:38:39 -0600

    Anus

--- src/eynewz/Main.java
+++ src/eynewz/Main.java
@@ -1,8 +1,22 @@
 package eynewz;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        String baseUrl = "https://csgofast.com/#history/double/all";
+        String tagName = "";
+
+        driver.get(baseUrl);
+        tagName = driver.findElement(By.tagName("body")).getText(); ;
+        System.out.println(tagName);
+        driver.close();
+        System.exit(0);
     }
 }
