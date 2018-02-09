49a513f196099824aceccefcba2aaf85cf62f45f
Tetonci0 <roberm98a@gmail.com>
2018-02-08 21:18:30 -0500

    To Create Branch
    Anus

---
+++ src/DataStructures/TetoGraph/Edges.java
@@ -0,0 +1,4 @@
+package DataStructures.TetoGraph;
+
+public class Edges {
+}
---
+++ src/algorithms/files/txt/txtAlgorithms.java
@@ -0,0 +1,24 @@
+package algorithms.files.txt;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+public class txtAlgorithms {
+
+    //Creates txt file, and saves it. Warning: If file already exists it would overwrite it.
+    public static void saveTxt(String txtPath, String text){
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("the-file-name.txt", "UTF-8");
+            writer.print(text);
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
